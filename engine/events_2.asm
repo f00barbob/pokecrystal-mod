abb51d792afec86b563a40b3e1ad87ed94d5b090
f00barbob <f00barbob@gmail.com>
2015-08-12 14:56:44 -0500

    Bug catching contest mon RNGesus

--- engine/events_2.asm
+++ engine/events_2.asm
@@ -249,16 +249,16 @@ Function97d64: ; 97d64
 
 ContestMons: ; 97d87
 	;   %, species,   min, max
-	db 20, CATERPIE,    7, 18
+	db 20, PICHU,    7, 18
 	db 20, WEEDLE,      7, 18
 	db 10, METAPOD,     9, 18
-	db 10, KAKUNA,      9, 18
-	db  5, BUTTERFREE, 12, 15
-	db  5, BEEDRILL,   12, 15
-	db 10, VENONAT,    10, 16
-	db 10, PARAS,      10, 17
-	db  5, SCYTHER,    13, 14
-	db  5, PINSIR,     13, 14
+	db 10, CLEFFA,      9, 18
+	db  5, GRIMER, 12, 15
+	db  5, YANMA,   12, 15
+	db 10, REMORAID,   10, 16
+	db 10, SPEAROW,    10, 17
+	db  5, KINGDRA,    13, 14
+	db  5, MR__MIME,   13, 14
 	db -1, VENOMOTH,   30, 40
 ; 97db3
 
